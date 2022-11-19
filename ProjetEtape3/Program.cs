0ad96c79010019b892729fa8bc6bd812153609c2
moustaphafal <tontonfall5@gmail.com>
2022-11-19 18:24:29 +0000

    Push initial

---
+++ ProjetEtape3/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    public static void main(String[] args)
+    {
+            Console.WriteLine("Donnez la borne inferieur");
+            String borne_inf = Console.ReadLine();
+            Console.WriteLine("Donnez la borne superieur");
+            String borne_sup = Console.ReadLine();
+            Console.WriteLine("Donnez le nombre de possibilite");
+            String Possibilite = Console.ReadLine();
+            int bf = 0, bs = 0, p = 0, choix = 0;
+            if (int.TryParse(borne_inf, out bf) && int.TryParse(borne_sup, out bs) && int.TryParse(Possibilite, out p))
+            {
+                Random rd = new Random();
+                int rand_num = rd.Next(bf, bs);
+                int tentative = 0;
+                do
+                {
+                    Console.WriteLine("Choississez un entre " + bf + " et " + bs);
+                    String nombre = Console.ReadLine();
+                    if (int.TryParse(nombre, out choix))
+                    {
+                        if (choix == rand_num)
+                        {
+                            Console.WriteLine("Vous avez gagnés");
+                            break;
+                        }
+                        else if (choix > rand_num)
+                        {
+                            Console.WriteLine("Le nombre est plus petit");
+                        }
+                        else if (choix < rand_num)
+                        {
+                            Console.WriteLine("le nombre est plus grand");
+                        }
+                        tentative++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choississez un nombre  !");
+
+                    }
+
+                } while (tentative < p);
+                int note = (p / tentative);
+                Console.WriteLine("Jeu Terminé ,Votre note est : " + note);
+            }
+            else
+            {
+                Console.WriteLine("Les valeurs renseignés sont incorrectes (bornes,possibilite)");
+            }
+            Console.Read();
+
+    }
+
+}
+
---
+++ TrouverLeNombre/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool arret = false;
+        int compteur = 10;
+        int[] resultats = new int[10] ;
+
+        while( compteur > 0 && arret == false)
+        {
+            codeSpaghetti :
+            Console.WriteLine("Il vous reste "+compteur+" tentatives");
+            Console.WriteLine("Hello, Veuillez entrer un nombre compris entre 1 et 10");
+            if (resultats == null)
+                Console.WriteLine("Le tableau est vide");
+            else
+            {
+                Console.Write("Les dernieres reponses : ");
+                foreach( int resultat in resultats)
+                {
+                    Console.Write(resultat+" ");
+                }
+                    Console.WriteLine();
+
+            }
+            string chaineInt = Console.ReadLine();
+            int nbreChoisis ;
+            int cp = 10 - compteur;
+
+            if (int.TryParse(chaineInt, out nbreChoisis))
+            {
+                //resultats.Append(nbreChoisis);
+                resultats[cp] = nbreChoisis;
+                if (nbreChoisis.Equals(5))
+                {
+                    Console.WriteLine(" Vous avez gagné !");
+                    arret= true;
+                }
+                else
+                {
+                    Console.WriteLine("Vous avez perdu !");
+
+                }
+            }
+            else
+            {
+                --compteur;
+                goto codeSpaghetti;
+            }
+
+            --compteur;
+
+        } 
+    }
+        
+}
